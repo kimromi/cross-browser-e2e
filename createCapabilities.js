a607a48d7593e4c8e546ec8e1214e13833acdb08
kimromi <kimromi4@gmail.com>
2019-10-24 10:16:04 +0900

    add ie for js

--- createCapabilities.js
+++ createCapabilities.js
@@ -14,6 +14,16 @@ if (process.env.BROWSER === 'chrome') {
   });
 } else if (process.env.BROWSER === 'safari') {
   capabilities = webdriver.Capabilities.safari();
+} else if (process.env.BROWSER === 'ie') {
+  // HACK: include IEDriver path by nuget
+  const driverPath = path.join(
+    __dirname,
+    "../Selenium.WebDriver.IEDriver.3.150.0/driver/"
+  );
+  process.env.PATH = `${process.env.PATH};${driverPath};`;
+  capabilities = webdriver.Capabilities.ie();
+  capabilities.set("ignoreProtectedModeSettings", true);
+  capabilities.set("ignoreZoomSetting", true);
 }
 
 module.exports = capabilities;
